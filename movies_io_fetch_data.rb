3835ae1319eb325f13699d66fae81cedfdae76d7
Ismael Abreu <ismaelga@gmail.com>
2012-09-07 10:37:25 +0100

    utilizados os metodos de scrab

--- movies_io_fetch_data.rb
+++ movies_io_fetch_data.rb
@@ -9,34 +9,62 @@ class MoviesIoFetchData
     puts 'Doing hard work ---> ' + url
     @url = url
     @page = Nokogiri.HTML open(@url)
+
+    scrab_cebecalho
+    scrab_informacao
+    scrab_downloads
+    save_to_db
   end
 
 private
-  def cabecalho
-  	cabecalho = @page.css('.movie_headline')
-  	@titulo = cabecalho.css('.movie_title').first.content
-  	@ano = cabecalho.css('.movie_year a').first.content
-  	@duracao = cabecalho.css('.movie_runtime').first.content
+  def scrab_cebecalho
+    cabecalho = @page.css('.movie_headline')
+    @titulo = cabecalho.css('.movie_title').first.content
+    @ano = cabecalho.css('.movie_year a').first.content
+    @duracao = cabecalho.css('.movie_runtime').first.content
   end
 
-  def informacao
-  	info = @page.css('.movie_info')
-  	@descricao = info.css('.trailer_complement .movie_plot').first['data-full-plot']
-  	@rating = info.css('.movie_rating .movie_rating_value').first.content
+  def scrab_informacao
+    info = @page.css('.movie_info')
+    @descricao = info.css('.trailer_complement .movie_plot').first['data-full-plot']
+    @rating = info.css('.movie_rating .movie_rating_value').first.content
   end
 
-  def downloads
-  	dowloads = @page.css('.sources table')
-  	@downloads = downloads.first.children.map do |downloa_line|
-  		download_data = {}
-  		download_data.tap do |d|
-  			d[:torrent_link] = download_line.css('.download-link-container a').first[:href]
-  			d[:file_quality] = download_line.css('.qu span').first.content
-  			d[:file_size] = download_line.css('.si').first.content
-  			d[:seeders] = download_line.css('.se').first.content
-  			d[:seeders] = download_line.css('.le').first.content
-  			d[:legendas] = download_line.css('.sub a').first[:href]
-  		end
-  	end
+  def scrab_downloads
+    downloads = @page.css('.sources table')
+    @downloads = downloads.first.children.map do |download_line|
+      download_data = {}
+      download_data.tap do |d|
+        dl = download_line.css('.download-link-container a')
+        if dl.any?
+           d[:torrent_link] = download_line.css('.download-link-container a').first[:href]
+           d[:file_quality] = download_line.css('.qu span').first.content
+           d[:file_size] = download_line.css('.si').first.content
+           d[:seeders] = download_line.css('.se').first.content
+           d[:seeders] = download_line.css('.le').first.content
+           d[:legendas] = download_line.css('.sub a').first[:href]
+         end
+      end
+    end
+  end
+
+  def save_to_db
+    # TODO
+    imprime
+  end
+
+  def imprime
+    puts '------------------------------------------------------------'
+    puts @titulo
+    puts @ano
+    puts @duracao
+    puts ''
+    puts @rating
+
+    puts '______________________'
+    @downloads.each {|d| puts "#{d[:file_quality]} - #{d[:file_size]}"}
+    puts '______________________'
+
+    puts '------------------------------------------------------------'
   end
 end
--- movies_io_fetcher_top_movies.rb
+++ movies_io_fetcher_top_movies.rb
@@ -5,15 +5,15 @@ class MoviesIoFetchTopMovies
   # include Sidekiq::Worker
 
   def perform
-  	@url = 'http://movies.io/i/top50'
+    @url = 'http://movies.io/i/top50'
     @page = Nokogiri.HTML open(@url)
     fetch_top50.each do |movie_url|
-    	MoviesIOFetchData.new.perform movie_url
+      MoviesIoFetchData.new.perform movie_url
     end
   end
 
 private
   def fetch_top50
-  	@movies_urls = @page.css('a.goodie').map { |e| "http://movies.io" + e[:href] }
+    @movies_urls = @page.css('a.goodie').map { |e| "http://movies.io" + e[:href] }
   end
 end
