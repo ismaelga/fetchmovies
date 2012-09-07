3ac185d80a616b2030f4ed4dca8111f6e3e9d914
Ismael Abreu <ismaelga@gmail.com>
2012-09-07 10:40:05 +0100

    formatacao

--- teste.rb
+++ teste.rb
@@ -5,16 +5,17 @@ class MoviesIoFetchTopMovies
   # include Sidekiq::Worker
 
   def perform
-  	@url = 'http://movies.io/i/top50'
+    @url = 'http://movies.io/i/top50'
     @page = Nokogiri.HTML open(@url)
     fetch_top50.each do |movie_url|
-    	MoviesIoFetchData.new.perform movie_url
+      MoviesIoFetchData.new.perform movie_url
     end
   end
 
-private
+  private
+
   def fetch_top50
-  	@movies_urls = @page.css('a.goodie').map { |e| "http://movies.io" + e[:href] }
+    @movies_urls = @page.css('a.goodie').map { |e| "http://movies.io" + e[:href] }
   end
 end
 
@@ -44,36 +45,37 @@ class MoviesIoFetchData
     save_to_db
   end
 
-private
+  private
+
   def scrab_cebecalho
-  	cabecalho = @page.css('.movie_headline')
-  	@titulo = cabecalho.css('.movie_title').first.content
-  	@ano = cabecalho.css('.movie_year a').first.content
-  	@duracao = cabecalho.css('.movie_runtime').first.content
+    cabecalho = @page.css('.movie_headline')
+    @titulo = cabecalho.css('.movie_title').first.content
+    @ano = cabecalho.css('.movie_year a').first.content
+    @duracao = cabecalho.css('.movie_runtime').first.content
   end
 
   def scrab_informacao
-  	info = @page.css('.movie_info')
-  	@descricao = info.css('.trailer_complement .movie_plot').first['data-full-plot']
-  	@rating = info.css('.movie_rating .movie_rating_value').first.content
+    info = @page.css('.movie_info')
+    @descricao = info.css('.trailer_complement .movie_plot').first['data-full-plot']
+    @rating = info.css('.movie_rating .movie_rating_value').first.content
   end
 
   def scrab_downloads
-  	downloads = @page.css('.sources table')
-  	@downloads = downloads.first.children.map do |download_line|
-  		download_data = {}
-  		download_data.tap do |d|
+    downloads = @page.css('.sources table')
+    @downloads = downloads.first.children.map do |download_line|
+      download_data = {}
+      download_data.tap do |d|
         dl = download_line.css('.download-link-container a')
         if dl.any?
-  			   d[:torrent_link] = download_line.css('.download-link-container a').first[:href]
-  			   d[:file_quality] = download_line.css('.qu span').first.content
-  			   d[:file_size] = download_line.css('.si').first.content
-  			   d[:seeders] = download_line.css('.se').first.content
-  			   d[:seeders] = download_line.css('.le').first.content
-  			   d[:legendas] = download_line.css('.sub a').first[:href]
-         end
-  		end
-  	end
+          d[:torrent_link] = download_line.css('.download-link-container a').first[:href]
+          d[:file_quality] = download_line.css('.qu span').first.content
+          d[:file_size] = download_line.css('.si').first.content
+          d[:seeders] = download_line.css('.se').first.content
+          d[:seeders] = download_line.css('.le').first.content
+          d[:legendas] = download_line.css('.sub a').first[:href]
+        end
+      end
+    end
   end
 
   def save_to_db
